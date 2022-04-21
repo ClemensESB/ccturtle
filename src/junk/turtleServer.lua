d286ea285fba2d6141e6d1ca2d89fb9e585a6a88
ClemensESB <clm.st.berger@gmail.com>
2022-04-21 15:25:01 +0200

    Update turtleServer.lua

--- src/junk/turtleServer.lua
+++ src/junk/turtleServer.lua
@@ -2,14 +2,14 @@ require("ccModem")
 require("ccMessage")
 
 local modem = ccModem:create()
-
+local function print_r(array)
+    for i, v in pairs(array) do
+        print(string.format("%s: %s", i, v))
+    end
+end
 modem:listenOn(128)
 while true do
     local turtleInformation = modem:receive()
     shell.run("clear")
-    local function print_r(array)
-        for i, v in pairs(array) do
-            print(string.format("%s: %s", i, v))
-        end
-    end
+    print_r(turtleInformation)
 end
