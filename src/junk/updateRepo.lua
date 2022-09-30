8e6ee027309a421d89435453f1b0c5c58799fcc7
ClemensESB <clm.st.berger@gmail.com>
2022-09-30 16:47:49 +0200

    Update updateRepo.lua

--- src/junk/updateRepo.lua
+++ src/junk/updateRepo.lua
@@ -1,10 +1,11 @@
 LINKS = {
-    "https://raw.githubusercontent.com/ClemensESB/ccturtle/main/src/quarry/buildJob.lua",
-    "https://raw.githubusercontent.com/ClemensESB/ccturtle/main/src/quarry/ccturtleMK3.lua",
-    "https://raw.githubusercontent.com/ClemensESB/ccturtle/main/src/junk/ccMessage.lua",
-    "https://raw.githubusercontent.com/ClemensESB/ccturtle/main/src/junk/ccModem.lua"
+    ["buildJob"] = "https://raw.githubusercontent.com/ClemensESB/ccturtle/main/src/quarry/buildJob.lua",
+    ["ccturtleMK3"] = "https://raw.githubusercontent.com/ClemensESB/ccturtle/main/src/quarry/ccturtleMK3.lua",
+    ["ccMessage"] = "https://raw.githubusercontent.com/ClemensESB/ccturtle/main/src/junk/ccMessage.lua",
+    ["ccModem"] = "https://raw.githubusercontent.com/ClemensESB/ccturtle/main/src/junk/ccModem.lua"
 }
 
 for k,v in pairs(LINKS) do
+    shell.run("rm "..k..".lua")
     shell.run("wget "..v)
 end
